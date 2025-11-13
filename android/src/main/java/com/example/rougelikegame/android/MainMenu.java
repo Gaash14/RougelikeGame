61805f7787a2a810792c2b41835426b6fb3cf8b5
Gaash14 <gaash14@gmail.com>
2025-11-13 18:40:58 +0200

    Initial commit

---
+++ android/src/main/java/com/example/rougelikegame/android/MainMenu.java
@@ -0,0 +1,26 @@
+package com.example.rougelikegame.android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.rougelikegame.R;
+
+public class MainMenu extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        Button startGame = findViewById(R.id.startGameButton);
+        Button exitButton = findViewById(R.id.exitButton);
+
+        startGame.setOnClickListener(v -> {
+            Intent intent = new Intent(this, AndroidLauncher.class);
+            startActivity(intent);
+        });
+
+        exitButton.setOnClickListener(v -> finishAffinity());
+    }
+}
