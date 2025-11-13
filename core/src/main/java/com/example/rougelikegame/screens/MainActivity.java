e92914c8f2b09c4c54c1803a4fe83a0f120e4faa
Gaash14 <gaash14@gmail.com>
2025-11-13 19:01:45 +0200

    Added Firebase

--- core/src/main/java/com/example/rougelikegame/screens/MainActivity.java
+++ core/src/main/java/com/example/rougelikegame/screens/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.rougelikegame.screens;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Array;
